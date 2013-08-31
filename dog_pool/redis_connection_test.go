87c351632840d274a42a20da3f9a982240d85a87
Glenn Nagel <glenn@mercury-wireless.com>
2013-08-31 09:30:31 -0400

    BitOp ComplementSet - Batch Redis Commands is 390% faster than individual Redis Connection Commands

--- dog_pool/redis_connection_test.go
+++ dog_pool/redis_connection_test.go
@@ -1,5 +1,6 @@
 package dog_pool
 
+import "fmt"
 import "testing"
 import "github.com/orfjackal/gospec/src/gospec"
 import "github.com/alecthomas/log4go"
@@ -224,5 +225,89 @@ func Benchmark_RedisConnection_BitOp_Not(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		server.Connection().Cmd("BITOP", "NOT", "ALL", "BOB", "Not-BOB", "GARY", "Cache-Miss")
+	}
+}
+
+//
+// Benchmark Bit Operation A & !B on 10x keys
+//
+func Benchmark_RedisConnection_BitOp_ComplementSet(b *testing.B) {
+	logger := log4go.NewDefaultLogger(log4go.CRITICAL)
+	server, err := StartRedisServer(&logger)
+	if nil != err {
+		panic(err)
+	}
+	defer server.Close()
+
+	keys_all := [][]string{make([]string, 10), make([]string, 10)}
+	keys_bob := [][]string{make([]string, 10), make([]string, 10)}
+	keys_not_bob := [][]string{make([]string, 10), make([]string, 10)}
+	keys_gary := [][]string{make([]string, 10), make([]string, 10)}
+	keys_not_gary := [][]string{make([]string, 10), make([]string, 10)}
+	for i := 0; i < 10; i++ {
+		for j := 0; j <= 1; j++ {
+			keys_all[j][i] = fmt.Sprintf("ALL:%d:%d", j, i)
+			keys_bob[j][i] = fmt.Sprintf("BOB:%d:%d", j, i)
+			keys_not_bob[j][i] = fmt.Sprintf("Not-BOB:%d:%d", j, i)
+			keys_gary[j][i] = fmt.Sprintf("GARY:%d:%d", j, i)
+			keys_not_gary[j][i] = fmt.Sprintf("Not-GARY:%d:%d", j, i)
+		}
+	}
+
+	for i := 0; i < 1024; i++ {
+		for j := 0; j < 10; j++ {
+			for k := 0; k <= 1; k++ {
+				server.Connection().Cmd("SETBIT", keys_all[k][j], i, true)
+				server.Connection().Cmd("SETBIT", keys_bob[k][j], i, i%2 == 0)
+				server.Connection().Cmd("SETBIT", keys_not_bob[k][j], i, i%2 == 1)
+				server.Connection().Cmd("SETBIT", keys_gary[k][j], i, i%4 == 0)
+				server.Connection().Cmd("SETBIT", keys_not_gary[k][j], i, i%4 == 0)
+			}
+		}
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buffer := []string{}
+		buffer = []string{"AND", "ALL:0"}
+		buffer = append(buffer, keys_all[0]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "ALL:0"}
+		buffer = append(buffer, keys_all[0]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "ALL:1"}
+		buffer = append(buffer, keys_all[1]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "BOB:0"}
+		buffer = append(buffer, keys_bob[0]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "BOB:1"}
+		buffer = append(buffer, keys_bob[1]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "Not-BOB:0"}
+		buffer = append(buffer, keys_not_bob[0]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "Not-BOB:1"}
+		buffer = append(buffer, keys_not_bob[1]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "GARY:0"}
+		buffer = append(buffer, keys_gary[0]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "GARY:1"}
+		buffer = append(buffer, keys_gary[1]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"AND", "Not-GARY:0"}
+		buffer = append(buffer, keys_not_gary[0]...)
+		server.Connection().Cmd("BITOP", buffer)
+		buffer = []string{"OR", "Not-GARY:1"}
+		buffer = append(buffer, keys_not_gary[1]...)
+		server.Connection().Cmd("BITOP", buffer)
+		server.Connection().Cmd("BITOP", "NOT", "Not-GARY:1", "Not-GARY:1")
+		server.Connection().Cmd("BITOP", "AND", "Complement", "Not-GARY:1", "ALL:0", "ALL:1", "BOB:0", "BOB:1", "Not-BOB:0", "Not-BOB:1", "GARY:0", "GARY:1", "Not-GARY:0")
+
+		for _, key := range []string{"Complement", "Not-GARY:1", "ALL:0", "ALL:1", "BOB:0", "BOB:1", "Not-BOB:0", "Not-BOB:1", "GARY:0", "GARY:1", "Not-GARY:0"} {
+			server.Connection().Cmd("BITCOUNT", key)
+		}
+		server.Connection().Cmd("GET", "Complement")
 	}
 }
